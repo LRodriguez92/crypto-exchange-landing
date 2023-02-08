a5eb566afe4d360ea06ba1388f7f0f156a4fc22a
Leo <Leonardo.Rodriguez92@gmail.com>
2023-02-08 11:49:28 -0500

    add crypto icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from './components/Hero';
 import Header from './components/Header';
 import Features from './components/Features';
+import ExchangeTable from './components/ExchangeTable';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Hero />
       <Features />
+      <ExchangeTable />
     </div>
   );
 }
---
+++ src/components/ExchangeTable.js
@@ -0,0 +1,124 @@
+const people = [
+    {
+      name: 'Lindsay Walton',
+      title: 'Front-end Developer',
+      department: 'Optimization',
+      email: 'lindsay.walton@example.com',
+      role: 'Member',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn4.iconfinder.com%2Fdata%2Ficons%2Flogos-and-brands%2F512%2F45_Bitcoin_logo_logos-512.png&f=1&nofb=1&ipt=cf23ea6fa1543cb950134f3e8b6c198d23100942f5fe1d43ee7941aacd5d378c&ipo=images',
+    },
+    {
+      name: 'Lindsay Walton',
+      title: 'Front-end Developer',
+      department: 'Optimization',
+      email: 'lindsay.walton@example.com',
+      role: 'Member',
+      image:
+        'https://cdn.iconscout.com/icon/free/png-512/ethereum-3521413-2944857.png',
+    },
+    {
+      name: 'Lindsay Walton',
+      title: 'Front-end Developer',
+      department: 'Optimization',
+      email: 'lindsay.walton@example.com',
+      role: 'Member',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.icons8.com%2Fcolor%2F2x%2Ftether.png&f=1&nofb=1&ipt=2c3eda005bde711ef0e708b70587126adf0a93f14f8591e327308605b20497a6&ipo=images',
+    },
+    {
+      name: 'Lindsay Walton',
+      title: 'Front-end Developer',
+      department: 'Optimization',
+      email: 'lindsay.walton@example.com',
+      role: 'Member',
+      image:
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2018%2F04%2FDogeCoin_logo_dog.png&f=1&nofb=1&ipt=224a776e6266b625c44b56ce76b07283774a288b922157e93603e45db97b472f&ipo=images',
+    },
+    // More people...
+  ]
+  
+  export default function ExchangeTable() {
+    return (
+      <div className="px-4 sm:px-6 lg:px-8 mx-auto max-w-7xl mt-28">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-xl font-semibold text-gray-900">Users</h1>
+            <p className="mt-2 text-sm text-gray-700">
+              A list of all the users in your account including their name, title, email and role.
+            </p>
+          </div>
+          <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+            >
+              Add user
+            </button>
+          </div>
+        </div>
+        <div className="mt-8 flex flex-col">
+          <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden md:rounded-lg">
+                <table className="min-w-full divide-y divide-transparent">
+                  <thead className="bg-transparent">
+                    <tr>
+                      <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                        Name
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Title
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Status
+                      </th>
+                      <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                        Role
+                      </th>
+                      <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                        <span className="sr-only">Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-[#a999b42c] bg-transparent">
+                    {people.map((person) => (
+                      <tr key={person.email}>
+                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+                          <div className="flex items-center">
+                            <div className="h-10 w-10 flex-shrink-0 rounded-full bg-black flex justify-center items-center">
+                              <img className="h-6 w-6  rounded-full" src={person.image} alt="" />
+                            </div>
+                            <div className="ml-4">
+                              <div className="font-medium text-gray-900">{person.name}</div>
+                              <div className="text-gray-500">{person.email}</div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <div className="text-gray-900">{person.title}</div>
+                          <div className="text-gray-500">{person.department}</div>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                          <span className="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">
+                            Active
+                          </span>
+                        </td>
+                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{person.role}</td>
+                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                          <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                            Edit<span className="sr-only">, {person.name}</span>
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
